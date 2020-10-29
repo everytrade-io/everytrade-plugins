9425fc3f7037ceecafeb91306b279cf7570f6b33
charvam <michael.charvat@everytrade.io>
2020-10-29 12:03:28 +0100

    ET-671 Updated XChange version, updated Huobi API connector, added unit tests.

---
+++ plugin-base/src/test/java/io/everytrade/server/plugin/impl/everytrade/HuobiDownloaderTest.java
@@ -0,0 +1,122 @@
+package io.everytrade.server.plugin.impl.everytrade;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.trade.UserTrade;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HuobiDownloaderTest {
+
+    private static UserTrade userTrade0, userTrade1, userTrade2, userTrade3, userTradeOldest;
+
+    @BeforeAll
+    static void prepareUserTrades() {
+        final Instant now = Instant.now();
+        userTrade3 = HuobiTestUtils.createUserTrade("3", Date.from(now), CurrencyPair.LTC_USD);
+        userTrade2 = HuobiTestUtils.createUserTrade("2", Date.from(now.minusSeconds(1)), CurrencyPair.LTC_USD);
+        userTrade1 = HuobiTestUtils.createUserTrade("1", Date.from(now.minusSeconds(2)), CurrencyPair.LTC_USD);
+        userTrade0 = HuobiTestUtils.createUserTrade("0", Date.from(now.minusSeconds(3)), CurrencyPair.LTC_BTC);
+        userTradeOldest = HuobiTestUtils.createUserTrade(
+            "0", Date.from(now.minus(HuobiDownloadState.DAYS_AGO, ChronoUnit.DAYS)), CurrencyPair.LTC_USD
+        );
+    }
+
+
+    @Test
+    void downloadNoNewTxsStartEvenDaysAgo()  {
+        final List<UserTrade> userTrades = List.of();
+        final HuobiTradeServiceMock tradeService = new HuobiTradeServiceMock(userTrades);
+        final String lastTx = String.format("LTC/USD=%s:::", LocalDate.now().minusDays(2));
+        final HuobiDownloader huobiDownloader = new HuobiDownloader(tradeService, lastTx);
+        final List<UserTrade> expected = List.of();
+        final List<UserTrade> downloaded = huobiDownloader.download("LTC/USD");
+        downloaded.sort(HuobiTestUtils::compareDesc);
+        assertTrue(HuobiTestUtils.checkAll(expected, downloaded));
+        final String expectedTx = String.format("LTC/USD=%s:::", LocalDate.now());
+        final String actual = huobiDownloader.getLastTransactionId();
+        assertEquals(actual, expectedTx);
+    }
+
+    @Test
+    void downloadNoNewTxsStartOddDaysAgo()  {
+        final List<UserTrade> userTrades = List.of();
+        final HuobiTradeServiceMock tradeService = new HuobiTradeServiceMock(userTrades);
+        final String lastTx = String.format("LTC/USD=%s:::", LocalDate.now().minusDays(3));
+        final HuobiDownloader huobiDownloader = new HuobiDownloader(tradeService, lastTx);
+        final List<UserTrade> expected = List.of();
+        final List<UserTrade> downloaded = huobiDownloader.download("LTC/USD");
+        downloaded.sort(HuobiTestUtils::compareDesc);
+        assertTrue(HuobiTestUtils.checkAll(expected, downloaded));
+        final String expectedTx = String.format("LTC/USD=%s:::", LocalDate.now().minusDays(1));
+        final String actual = huobiDownloader.getLastTransactionId();
+        assertEquals(actual, expectedTx);
+    }
+
+    @Test
+    void downloadWithLastId()  {
+        final List<UserTrade> userTrades = List.of(userTrade3, userTrade2, userTrade1);
+        final HuobiTradeServiceMock tradeService = new HuobiTradeServiceMock(userTrades);
+        final HuobiDownloader huobiDownloader
+            = new HuobiDownloader(tradeService, String.format("LTC/USD=%s:1::", LocalDate.now()));
+        final List<UserTrade> expected = List.of(userTrade3, userTrade2);
+        final List<UserTrade> downloaded = huobiDownloader.download("LTC/USD");
+        downloaded.sort(HuobiTestUtils::compareDesc);
+        assertTrue(HuobiTestUtils.checkAll(expected, downloaded));
+        final String expectedTx = String.format("LTC/USD=%s:3::", LocalDate.now());
+        final String actual = huobiDownloader.getLastTransactionId();
+        assertEquals(actual, expectedTx);
+    }
+
+    @Test
+    void downloadWithGap()  {
+        final List<UserTrade> userTrades = List.of(userTrade3, userTrade2, userTrade1);
+        final HuobiTradeServiceMock tradeService = new HuobiTradeServiceMock(userTrades);
+        final HuobiDownloader huobiDownloader
+            = new HuobiDownloader(tradeService, String.format("LTC/USD=%s::2:3", LocalDate.now()));
+        final List<UserTrade> expected = List.of(userTrade1);
+        final List<UserTrade> downloaded = huobiDownloader.download("LTC/USD");
+        downloaded.sort(HuobiTestUtils::compareDesc);
+        assertTrue(HuobiTestUtils.checkAll(expected, downloaded));
+        final String expectedTx = String.format("LTC/USD=%1$s:3::", LocalDate.now());
+        final String actual = huobiDownloader.getLastTransactionId();
+        assertEquals(actual, expectedTx);
+    }
+
+    @Test
+    void downloadNoLastTxId()  {
+        final List<UserTrade> userTrades = List.of(userTradeOldest);
+        final HuobiTradeServiceMock tradeService = new HuobiTradeServiceMock(userTrades);
+        final HuobiDownloader huobiDownloader
+            = new HuobiDownloader(tradeService, null);
+        final List<UserTrade> expected = tradeService.getUserTrades();
+        final List<UserTrade> downloaded = huobiDownloader.download("LTC/USD");
+        downloaded.sort(HuobiTestUtils::compareDesc);
+        assertTrue(HuobiTestUtils.checkAll(expected, downloaded));
+    }
+
+    @Test
+    void downloadTwoPairs()  {
+        final List<UserTrade> userTrades = List.of(userTrade3, userTrade2, userTrade1, userTrade0);
+        final HuobiTradeServiceMock tradeService = new HuobiTradeServiceMock(userTrades);
+        final HuobiDownloader huobiDownloader = new HuobiDownloader(
+            tradeService,
+            String.format("LTC/USD=%1$s:3::|LTC/BTC=%1$s:0::", LocalDate.now().minusDays(2))
+        );
+        final List<UserTrade> expected = tradeService.getUserTrades();
+        final List<UserTrade> downloaded = huobiDownloader.download("LTC/USD, LTC/BTC");
+        downloaded.sort(HuobiTestUtils::compareDesc);
+        assertTrue(HuobiTestUtils.checkAll(expected, downloaded));
+        final String expectedTx = String.format("LTC/USD=%1$s:3::|LTC/BTC=%1$s:0::", LocalDate.now());
+        final String actual = huobiDownloader.getLastTransactionId();
+        assertEquals(actual, expectedTx);
+    }
+}
---
+++ plugin-base/src/test/java/io/everytrade/server/plugin/impl/everytrade/HuobiTradeServiceMock.java
@@ -0,0 +1,66 @@
+package io.everytrade.server.plugin.impl.everytrade;
+
+import org.knowm.xchange.dto.marketdata.Trades;
+import org.knowm.xchange.dto.trade.UserTrade;
+import org.knowm.xchange.dto.trade.UserTrades;
+import org.knowm.xchange.huobi.service.HuobiTradeHistoryParams;
+import org.knowm.xchange.service.trade.TradeService;
+import org.knowm.xchange.service.trade.params.TradeHistoryParams;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class HuobiTradeServiceMock implements TradeService {
+    private final List<UserTrade> userTrades;
+
+    public HuobiTradeServiceMock(List<UserTrade> userTrades) {
+        this.userTrades = List.copyOf(userTrades);
+    }
+
+    public List<UserTrade> getUserTrades() {
+        return List.copyOf(userTrades);
+    }
+
+    @Override
+    public UserTrades getTradeHistory(TradeHistoryParams params) {
+        HuobiTradeHistoryParams huobiParams = (HuobiTradeHistoryParams) params;
+        final List<UserTrade> filteredUserTrades = userTrades
+            .stream()
+            .filter(ut -> ut.getInstrument().equals(huobiParams.getCurrencyPair()))
+            .filter(ut -> isDateIn(ut.getTimestamp(), huobiParams.getStartTime(), huobiParams.getEndTime()))
+            .filter(ut -> isLessOrEqual(ut.getOrderId(), huobiParams.getStartId()))
+            .limit(100)
+            .collect(Collectors.toList());
+        return new UserTrades(filteredUserTrades, Trades.TradeSortType.SortByTimestamp);
+    }
+
+    private boolean isDateIn(Date userTrade, Date paramsFrom, Date paramsTo) {
+        final LocalDate convertUserTrade = convert(userTrade);
+        final LocalDate convertParamsFrom = convert(paramsFrom);
+        final LocalDate convertParamsTo = convert(paramsTo);
+        return convertUserTrade.compareTo(convertParamsFrom) > -1 && convertUserTrade.compareTo(convertParamsTo) < 1;
+    }
+
+    private LocalDate convert(Date date) {
+        return date.toInstant()
+            .atZone(ZoneId.systemDefault())
+            .toLocalDate();
+    }
+
+    private boolean isLessOrEqual(String userTradeIdOrder, String paramsIdOrder) {
+        Objects.requireNonNull(userTradeIdOrder);
+        if (paramsIdOrder == null) {
+            return true;
+        }
+        return Long.compare(Long.parseLong(userTradeIdOrder), Long.parseLong(paramsIdOrder)) < 1;
+    }
+
+    @Override
+    public TradeHistoryParams createTradeHistoryParams() {
+        return new HuobiTradeHistoryParams();
+    }
+}
