d86eb3f9cebb1999e909dc68d279d09436df3de6
charvam <michael.charvat@everytrade.io>
2020-12-09 13:22:16 +0100

    ET-746 Extended Huobi exchange CSV bean unit test.

--- plugin-base/src/test/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/bean/HuobiBeanV1Test.java
+++ plugin-base/src/test/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/bean/HuobiBeanV1Test.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
 
 import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.ExchangeBean.UNSUPPORTED_CURRENCY_PAIR;
@@ -69,57 +70,99 @@ class HuobiBeanV1Test {
         ParserTestUtils.checkEqual(expected, actual);
     }
 
-//    @Test
-//    void testCorrectParsingRawTransactionBuyDiffFee() {
-//        final String row = "2020-03-31 21:31:43,Exchange,LTC/BTC,Buy,0.006040,0.8940,0.0053,0.00178800BTC,\n";
-//        final ImportedTransactionBean txBeanParsed = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
-//        final ImportedTransactionBean txBeanCorrect = new ImportedTransactionBean(
-//            null,
-//            Instant.parse("2020-03-31T21:31:43Z"),
-//            Currency.LTC,
-//            Currency.BTC,
-//            TransactionType.BUY,
-//            new BigDecimal("0.894"),
-//            new BigDecimal("0.0079284116"),
-//            new BigDecimal("0")
-//        );
-//        ParserTestUtils.checkEqual(txBeanParsed, txBeanCorrect);
-//    }
-//
-//
-//    @Test
-//    void testCorrectParsingRawTransactionSell() {
-//        final String row = "2020-03-31 21:31:24,Exchange,LTC/BTC,Sell,0.006036,0.7362,0.0044,0.00000888BTC,\n";
-//        final ImportedTransactionBean txBeanParsed = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
-//        final ImportedTransactionBean txBeanCorrect = new ImportedTransactionBean(
-//            null,
-//            Instant.parse("2020-03-31T21:31:24Z"),
-//            Currency.LTC,
-//            Currency.BTC,
-//            TransactionType.SELL,
-//            new BigDecimal("0.73620000"),
-//            new BigDecimal("0.0059645748"),
-//            new BigDecimal("0")
-//        );
-//        ParserTestUtils.checkEqual(txBeanParsed, txBeanCorrect);
-//    }
-//
-//    @Test
-//    void testCorrectParsingRawTransactionSellDiffFee() {
-//        final String row = "2020-03-31 21:31:24,Exchange,LTC/BTC,Sell,0.006036,0.7362,0.0044,0.00000888LTC,\n";
-//        final ImportedTransactionBean txBeanParsed = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
-//        final ImportedTransactionBean txBeanCorrect = new ImportedTransactionBean(
-//            null,
-//            Instant.parse("2020-03-31T21:31:24Z"),
-//            Currency.LTC,
-//            Currency.BTC,
-//            TransactionType.SELL,
-//            new BigDecimal("0.73620888"),
-//            new BigDecimal("0.0059765647"),
-//            new BigDecimal("0")
-//        );
-//        ParserTestUtils.checkEqual(txBeanParsed, txBeanCorrect);
-//    }
+    @Test
+    void testCorrectParsingRawTransactionBuyDiffFee() {
+        final String row = "2020-03-31 21:31:43,Exchange,LTC/BTC,Buy,0.006040,0.8940,0.0053,0.00178800BTC,\n";
+        final TransactionCluster actual = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
+        final TransactionCluster expected = new TransactionCluster(
+            new BuySellImportedTransactionBean(
+                null,
+                Instant.parse("2020-03-31T21:31:43Z"),
+                Currency.LTC,
+                Currency.BTC,
+                TransactionType.BUY,
+                new BigDecimal("0.8940"),
+                new BigDecimal("0.0059284116")
+            ),
+            List.of(
+                new FeeRebateImportedTransactionBean(
+                    "-fee",
+                    Instant.parse("2020-03-31T21:31:43Z"),
+                    Currency.LTC,
+                    Currency.BTC,
+                    TransactionType.FEE,
+                    new BigDecimal("0.00178800"),
+                    Currency.BTC
+                )
+            ),
+            0
+        );
+
+        ParserTestUtils.checkEqual(expected, actual);
+    }
+
+
+    @Test
+    void testCorrectParsingRawTransactionSell() {
+        final String row = "2020-03-31 21:31:24,Exchange,LTC/BTC,Sell,0.006036,0.7362,0.0044,0.00000888BTC,\n";
+        final TransactionCluster actual = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
+        final TransactionCluster expected = new TransactionCluster(
+            new BuySellImportedTransactionBean(
+                null,
+                Instant.parse("2020-03-31T21:31:24Z"),
+                Currency.LTC,
+                Currency.BTC,
+                TransactionType.SELL,
+                new BigDecimal("0.7362"),
+                new BigDecimal("0.0059766368")
+            ),
+            List.of(
+                new FeeRebateImportedTransactionBean(
+                    "-fee",
+                    Instant.parse("2020-03-31T21:31:24Z"),
+                    Currency.LTC,
+                    Currency.BTC,
+                    TransactionType.FEE,
+                    new BigDecimal("0.00000888"),
+                    Currency.BTC
+                )
+            ),
+            0
+        );
+
+        ParserTestUtils.checkEqual(expected, actual);
+    }
+
+    @Test
+    void testCorrectParsingRawTransactionSellDiffFee() {
+        final String row = "2020-03-31 21:31:24,Exchange,LTC/BTC,Sell,0.006036,0.7362,0.0044,0.00000888LTC,\n";
+        final TransactionCluster actual = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
+        final TransactionCluster expected = new TransactionCluster(
+            new BuySellImportedTransactionBean(
+                null,
+                Instant.parse("2020-03-31T21:31:24Z"),
+                Currency.LTC,
+                Currency.BTC,
+                TransactionType.SELL,
+                new BigDecimal("0.7362"),
+                new BigDecimal("0.0059766368")
+            ),
+            List.of(
+                new FeeRebateImportedTransactionBean(
+                    "-fee",
+                    Instant.parse("2020-03-31T21:31:24Z"),
+                    Currency.LTC,
+                    Currency.BTC,
+                    TransactionType.FEE,
+                    new BigDecimal("0.00000888"),
+                    Currency.LTC
+                )
+            ),
+            0
+        );
+
+        ParserTestUtils.checkEqual(expected, actual);
+    }
 
 
     @Test
@@ -140,22 +183,26 @@ class HuobiBeanV1Test {
         assertTrue(error.contains(UNSUPPORTED_TRANSACTION_TYPE.concat("Deposit")));
     }
 
-//    @Test
-//    void testWrongFeeCurrency() {
-//        final String row = "2020-03-31 21:31:43,Exchange,LTC/BTC,Buy,0.006040,0.8940,0.0053,0.00178800ETH,\n";
-//        final ImportedTransactionBean txBeanParsed = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
-//        final ImportedTransactionBean txBeanCorrect = new ImportedTransactionBean(
-//            null,
-//            Instant.parse("2020-03-31T21:31:43Z"),
-//            Currency.LTC,
-//            Currency.BTC,
-//            TransactionType.BUY,
-//            new BigDecimal("0.89400000"),
-//            new BigDecimal("0.0059284116"),
-//            new BigDecimal("0")
-//        );
-//        ParserTestUtils.checkEqual(txBeanParsed, txBeanCorrect);
-//    }
+    @Test
+    void testWrongFeeCurrency() {
+        final String row = "2020-03-31 21:31:43,Exchange,LTC/BTC,Buy,0.006040,0.8940,0.0053,0.00178800ETH,\n";
+        final TransactionCluster actual = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
+        final TransactionCluster expected = new TransactionCluster(
+            new BuySellImportedTransactionBean(
+                null,
+                Instant.parse("2020-03-31T21:31:43Z"),
+                Currency.LTC,
+                Currency.BTC,
+                TransactionType.BUY,
+                new BigDecimal("0.8940"),
+                new BigDecimal("0.0059284116")
+            ),
+            Collections.emptyList(),
+            1
+        );
+
+        ParserTestUtils.checkEqual(expected, actual);
+    }
 
     @Test
     void testNotAllowedPair() {
