a50b748328bd1730dc8d5808dac0d02f29b7d7a7
Radovan Panák <radovan.panak@everytrade.io>
2020-12-11 13:07:58 +0100

    enhance SupportedExchange

--- plugin-api/src/main/java/io/everytrade/server/model/SupportedExchange.java
+++ plugin-api/src/main/java/io/everytrade/server/model/SupportedExchange.java
@@ -1,5 +1,9 @@
 package io.everytrade.server.model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 //TODO: duplicate information with Exchange?
 public enum SupportedExchange {
     BITSTAMP("Bitstamp", "bitstamp"),
@@ -25,6 +29,21 @@ public enum SupportedExchange {
     private final String displayName;
     private final String internalId;
 
+    private static final Map<String, SupportedExchange> BY_ID;
+
+    static {
+        BY_ID = new HashMap<>();
+        for (SupportedExchange value : values()) {
+            final SupportedExchange previousMapping = BY_ID.put(value.internalId, value);
+            if (previousMapping != null) {
+                throw new IllegalStateException(
+                    String.format(
+                        "Multiple constants map to '%s': '%s', '%s'.", value.internalId, value, previousMapping)
+                );
+            }
+        }
+    }
+
     SupportedExchange(String displayName, String internalId) {
         this.displayName = displayName;
         this.internalId = internalId;
@@ -36,5 +55,9 @@ public enum SupportedExchange {
 
     public String getInternalId() {
         return internalId;
+    }
+
+    public static Optional<SupportedExchange> fromId(String id) {
+        return Optional.ofNullable(BY_ID.get(id));
     }
 }
