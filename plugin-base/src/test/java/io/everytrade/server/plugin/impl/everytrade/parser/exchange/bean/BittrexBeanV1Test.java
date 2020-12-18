fafac9a8dd15d0fc7bd44c2e614051a8a69888de
charvam <michael.charvat@everytrade.io>
2020-12-18 15:06:31 +0100

    ET-746 Added csv parser bean unit test. - Bitstamp, Bittrex

---
+++ plugin-base/src/test/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/bean/BittrexBeanV1Test.java
@@ -0,0 +1,83 @@
+package io.everytrade.server.plugin.impl.everytrade.parser.exchange.bean;
+
+import io.everytrade.server.model.Currency;
+import io.everytrade.server.model.TransactionType;
+import io.everytrade.server.plugin.api.parser.BuySellImportedTransactionBean;
+import io.everytrade.server.plugin.api.parser.FeeRebateImportedTransactionBean;
+import io.everytrade.server.plugin.api.parser.ParsingProblem;
+import io.everytrade.server.plugin.api.parser.TransactionCluster;
+import io.everytrade.server.plugin.impl.everytrade.parser.exception.ParsingProcessException;
+import io.everytrade.server.plugin.impl.everytrade.parser.exchange.ExchangeBean;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.ExchangeBean.FEE_UID_PART;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+class BittrexBeanV1Test {
+    private static final String HEADER_CORRECT
+        = "OrderUuid,Exchange,Type,Quantity,Limit,CommissionPaid,Price,Opened,Closed\n";
+
+    @Test
+    void testWrongHeader() {
+        final String headerWrong = "OrderUuid,Exchange,Type,Quantity,Limit,CommissionPaid,Price,Opened,ClXsed\n";
+        try {
+            ParserTestUtils.testParsing(headerWrong);
+            fail("No expected exception has been thrown.");
+        } catch (ParsingProcessException e) {
+        }
+    }
+
+    @Test
+    void testCorrectParsingRawTransaction()  {
+        final String row = "9-01,EUR-ETH,LIMIT_BUY,0.03280507,0.03380240,0.00000277," +
+            "0.00110889,2/14/19 15:01,2/14/19 15:01\n";
+        final TransactionCluster actual = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
+        final TransactionCluster expected = new TransactionCluster(
+            new BuySellImportedTransactionBean(
+                "9-01",
+                Instant.parse("2019-02-14T15:01:00Z"),
+                Currency.ETH,
+                Currency.EUR,
+                TransactionType.BUY,
+                new BigDecimal("0.03280507"),
+                new BigDecimal("0.0338023970")
+            ),
+            List.of(
+                new FeeRebateImportedTransactionBean(
+                    "9-01" + FEE_UID_PART,
+                    Instant.parse("2019-02-14T15:01:00Z"),
+                    Currency.ETH,
+                    Currency.EUR,
+                    TransactionType.FEE,
+                    new BigDecimal("0.00000277"),
+                    Currency.EUR
+                )
+            ),
+            0
+        );
+        ParserTestUtils.checkEqual(expected, actual);
+    }
+
+    @Test
+     void testUnknonwExchange() {
+        final String row = "9-01,ETH-BTK,LIMIT_BUY,0.03280507,0.03380240," +
+            "0.00000277,0.00110889,2/14/19 15:01,2/14/19 15:01\n";
+        final ParsingProblem parsingProblem = ParserTestUtils.getParsingProblem(HEADER_CORRECT + row);
+        final String error = parsingProblem.getMessage();
+        assertTrue(error.contains("ETH-BTK"));
+    }
+
+    @Test
+    void testIgnoredTransactionType() {
+        final String row = "9-01,EUR-ETH,BUY,0.03280507,0.03380240,0.00000277," +
+            "0.00110889,2/14/19 15:01,2/14/19 15:01\n";
+        final ParsingProblem parsingProblem = ParserTestUtils.getParsingProblem(HEADER_CORRECT + row);
+        assertTrue(parsingProblem.getMessage().contains(ExchangeBean.UNSUPPORTED_TRANSACTION_TYPE.concat("BUY")));
+    }
+}
---
+++ plugin-base/src/test/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/bean/BittrexBeanV2Test.java
@@ -0,0 +1,85 @@
+package io.everytrade.server.plugin.impl.everytrade.parser.exchange.bean;
+
+import io.everytrade.server.model.Currency;
+import io.everytrade.server.model.TransactionType;
+import io.everytrade.server.plugin.api.parser.BuySellImportedTransactionBean;
+import io.everytrade.server.plugin.api.parser.FeeRebateImportedTransactionBean;
+import io.everytrade.server.plugin.api.parser.ParsingProblem;
+import io.everytrade.server.plugin.api.parser.TransactionCluster;
+import io.everytrade.server.plugin.impl.everytrade.parser.exception.ParsingProcessException;
+import io.everytrade.server.plugin.impl.everytrade.parser.exchange.ExchangeBean;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.ExchangeBean.FEE_UID_PART;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class BittrexBeanV2Test {
+    private static final String HEADER_CORRECT
+        = "Uuid,Exchange,TimeStamp,OrderType,Limit,Quantity,QuantityRemaining,Commission,Price," +
+        "PricePerUnit,IsConditional,Condition,ConditionTarget,ImmediateOrCancel,Closed\n";
+
+    @Test
+    void testWrongHeader() {
+        String headerWrong = "Uuid,Exchang_,TimeStamp,OrderType,Limit,Quantity,QuantityRemaining,Commission,Price," +
+            "PricePerUnit,IsConditional,Condition,ConditionTarget,ImmediateOrCancel,Closed\n";
+        try {
+            ParserTestUtils.testParsing(headerWrong);
+            fail("No expected exception has been thrown.");
+        } catch (ParsingProcessException e) {
+        }
+    }
+
+    @Test
+    void testCorrectParsingRawTransaction() {
+        final String row = "d-01,USD-BTC,7/28/2019 1:35:53 PM,LIMIT_SELL,9471.45900000," +
+            "0.00321000,0.00000000,0.07600851,30.40340586,9471.46600000,False,,0.00000000,False,7/28/2019 1:35:53 PM\n";
+        final TransactionCluster actual = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
+        final TransactionCluster expected = new TransactionCluster(
+            new BuySellImportedTransactionBean(
+                "d-01",
+                Instant.parse("2019-07-28T13:35:53Z"),
+                Currency.BTC,
+                Currency.USD,
+                TransactionType.SELL,
+                new BigDecimal("0.00321"),
+                new BigDecimal("9471.466")
+            ),
+            List.of(
+                new FeeRebateImportedTransactionBean(
+                    "d-01" + FEE_UID_PART,
+                    Instant.parse("2019-07-28T13:35:53Z"),
+                    Currency.BTC,
+                    Currency.USD,
+                    TransactionType.FEE,
+                    new BigDecimal("0.07600851"),
+                    Currency.USD
+                )
+            ),
+            0
+        );
+        ParserTestUtils.checkEqual(expected, actual);
+    }
+
+    @Test
+    void testUnknonwExchange() {
+        final String row = "d-01,BTC-UUU,7/28/2019 1:35:53 PM,LIMIT_SELL," +
+            "9471.45900000,0.00321000,0.00000000,0.07600851,30.40340586,9471.46600000,False,,0.00000000,False," +
+            "7/28/2019 1:35:53 PM\n";
+        final ParsingProblem parsingProblem = ParserTestUtils.getParsingProblem(HEADER_CORRECT + row);
+        final String error = parsingProblem.getMessage();
+        assertTrue(error.contains("BTC-UUU"));
+    }
+
+    @Test
+    void testIgnoredTransactionType() {
+        final String row = "d-01,USD-BTC,7/28/2019 1:35:53 PM,BUY,9471.45900000," +
+            "0.00321000,0.00000000,0.07600851,30.40340586,9471.46600000,False,,0.00000000,False,7/28/2019 1:35:53 PM\n";
+        final ParsingProblem parsingProblem = ParserTestUtils.getParsingProblem(HEADER_CORRECT + row);
+        assertTrue(parsingProblem.getMessage().contains(ExchangeBean.UNSUPPORTED_TRANSACTION_TYPE.concat("BUY")));
+    }
+}
