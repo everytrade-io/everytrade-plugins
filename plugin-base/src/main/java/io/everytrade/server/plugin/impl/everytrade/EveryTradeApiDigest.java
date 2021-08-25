6c4c6849cbf45c7c0a1d3354317a458339fdc308
kubacech <kubacech@gmail.com>
2021-08-25 11:16:39 +0200

    ETD-47 changing cloud url for signature in GB connector

--- plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/EveryTradeApiDigest.java
+++ plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/EveryTradeApiDigest.java
@@ -21,7 +21,11 @@ public class EveryTradeApiDigest extends BaseParamsDigest {
 
         sha512Hmac.update(restInvocation.getHttpMethod().getBytes(StandardCharsets.UTF_8));
         sha512Hmac.update(SPACE);
-        sha512Hmac.update(restInvocation.getInvocationUrl().getBytes(StandardCharsets.UTF_8));
+
+        String url = restInvocation.getInvocationUrl();
+        url = url.replaceAll("cloud.generalbytes.com/", "cloud.generalbytes.com:7777/");
+
+        sha512Hmac.update(url.getBytes(StandardCharsets.UTF_8));
         if (restInvocation.getRequestBody() != null) {
             sha512Hmac.update(SPACE);
             sha512Hmac.update(restInvocation.getRequestBody().getBytes(StandardCharsets.UTF_8));
