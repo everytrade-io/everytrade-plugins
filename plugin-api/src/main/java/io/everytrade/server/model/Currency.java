13aef0156fcea07b31e87b6b853f4f53e31cbcb3
kubacech <kubacech@gmail.com>
2021-12-22 13:14:09 +0100

    ETD-184 adding currencies

--- plugin-api/src/main/java/io/everytrade/server/model/Currency.java
+++ plugin-api/src/main/java/io/everytrade/server/model/Currency.java
@@ -131,7 +131,15 @@ public enum Currency {
     VTC(false, Instant.parse("2014-01-08T00:00:00Z"), "Vertcoin"),
     ERG(false, Instant.parse("2017-07-02T00:00:00Z"), "Ergo"),
     LUNA(false, Instant.parse("2019-08-01T00:00:00Z"), "Terra"),
-    CLO(false, Instant.parse("2018-03-05T00:00:00Z"), "Callisto Network");
+    CLO(false, Instant.parse("2018-03-05T00:00:00Z"), "Callisto Network"),
+    ICP(false, Instant.parse("2021-05-12"), "Internet Computer"),
+    RLC(false, Instant.parse("2017-04-20"), "iExec RLC"),
+    FLOW(false, Instant.parse("2021-04-15"), "Flow"),
+    FTT(false, Instant.parse("2019-08-01"), "FTX Token"),
+    PERP(false, Instant.parse("2021-11-12"), "Perpetual Protocol"),
+    SRM(false, Instant.parse("2020-08-20"), "Serum"),
+    XEC(false, Instant.parse("2021-08-26"), "eCash"),
+    ;
 
     private final String code;
     private final int decimalDigits;
