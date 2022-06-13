8e4199a1f282bb72872ca3213ff64a11f1b8c57b
martinkyov <martin.kyovsky@generalbytes.com>
2022-06-13 12:55:37 +0200

    ETD-384:containers-row_parsing_failed

--- plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/binance/v3/BinanceExchangeSpecificParserV3.java
+++ plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/binance/v3/BinanceExchangeSpecificParserV3.java
@@ -3,6 +3,7 @@ package io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v3;
 import com.univocity.parsers.common.record.Record;
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
+import io.everytrade.server.model.CurrencyPair;
 import io.everytrade.server.plugin.api.parser.ParsingProblem;
 import io.everytrade.server.plugin.api.parser.ParsingProblemType;
 import io.everytrade.server.plugin.impl.everytrade.parser.exception.DataIgnoredException;
@@ -15,8 +16,11 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static io.everytrade.server.model.CurrencyPair.getTradeablePairs;
 import static io.everytrade.server.plugin.api.parser.ParsingProblemType.PARSED_ROW_IGNORED;
 import static io.everytrade.server.plugin.api.parser.ParsingProblemType.ROW_PARSING_FAILED;
 
@@ -24,13 +28,16 @@ public class BinanceExchangeSpecificParserV3 implements IExchangeSpecificParser 
     private static final String DEFAULT_DELIMITER = ",";
     private final String delimiter;
     private List<ParsingProblem> parsingProblems = List.of();
+    private static Map<String, CurrencyPair> fastCurrencyPair = new HashMap<>();
 
     public BinanceExchangeSpecificParserV3() {
         delimiter = DEFAULT_DELIMITER;
     }
 
-    public BinanceExchangeSpecificParserV3(String delimiter) {
-        this.delimiter = delimiter;
+    static {
+        getTradeablePairs().forEach(t -> fastCurrencyPair.put(
+            String.format("%s%s", t.getBase().code(), t.getQuote().code()), t)
+        );
     }
 
     @Override
@@ -44,7 +51,10 @@ public class BinanceExchangeSpecificParserV3 implements IExchangeSpecificParser 
             csvParserSettings.setHeaderExtractionEnabled(false);
             CsvParser parser = new CsvParser(csvParserSettings);
             List<Record> allRecords = parser.parseAllRecords(reader);
-
+            // remove row with header
+            if(!allRecords.isEmpty()) {
+                allRecords.remove(0);
+            }
             for (final Record record : allRecords) {
                 BinanceBeanV3 binanceBean = parseExchangeBean(record.getValues());
                 if (binanceBean != null) {
@@ -65,7 +75,7 @@ public class BinanceExchangeSpecificParserV3 implements IExchangeSpecificParser 
     private BinanceBeanV3 parseExchangeBean(String[] vals) {
         var row = String.format("%s,%s,%s,%s,%s,%s,%s", vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6]);
         try {
-            return new BinanceBeanV3(vals[0], vals[1], vals[2], vals[4], vals[5], vals[6]);
+            return new BinanceBeanV3(vals[0], vals[1], vals[2], vals[4], vals[5], vals[6], fastCurrencyPair);
         } catch (DataIgnoredException e) {
             parsingProblems.add(
                 new ParsingProblem(row, e.getMessage(), PARSED_ROW_IGNORED)
