2c50a7837ecbb0fb67bc7b35db05b37d65e51927
martinkyov <martin.kyovsky@generalbytes.com>
2022-10-07 15:59:05 +0200

    ETD-581:connectors-xpub-address after 1st code review

--- plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/BlockchainDownloader.java
+++ plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/BlockchainDownloader.java
@@ -115,7 +115,7 @@ public class BlockchainDownloader {
             page = 0;
         }
         if (isXpub(source)) {
-            final Collection<AddressInfo> addressInfos = client.getXpubTransactions(source, lastTxTimestamp, LIMIT, SORT_ASC);;
+            final Collection<AddressInfo> addressInfos = client.getAddressesInfoFromXpub(source, lastTxTimestamp, LIMIT, SORT_ASC);
             if (addressInfos == null) {
                 throw new IllegalArgumentException(String.format(
                     "No addresses info found for crypto '%s' and key '%s'",
