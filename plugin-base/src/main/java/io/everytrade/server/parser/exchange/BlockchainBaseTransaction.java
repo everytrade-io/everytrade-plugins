06bca9091f05c941156cca3a774ab64298d7f4a0
MartinKyovsky <martin.kyovsky@generalbytes.com>
2022-11-30 16:40:32 +0100

    bugfix/ETD-716-connectors-loading-separate-outg

---
+++ plugin-base/src/main/java/io/everytrade/server/parser/exchange/BlockchainBaseTransaction.java
@@ -0,0 +1,24 @@
+package io.everytrade.server.parser.exchange;
+
+import io.everytrade.server.model.Currency;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+public class BlockchainBaseTransaction {
+    String mainTransactionHash;
+    String trHash;
+    String relativeAddress;
+    String address;
+    long timestamp;
+    long receivedTimestamp;
+    BigDecimal originalValue;
+    BigDecimal originalFee;
+    BigDecimal fee;
+    BigDecimal value;
+    Currency currency;
+    boolean isTransactionSend;
+}
