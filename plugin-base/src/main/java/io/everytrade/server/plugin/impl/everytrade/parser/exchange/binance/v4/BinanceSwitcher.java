22214a111e6c1dade073dd6ecefc91f99ac4846c
martinkyov <martin.kyovsky@generalbytes.com>
2023-01-07 21:03:02 +0100

    ETD-775:containers-binance-csv---transac

--- plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/binance/v4/BinanceSwitcher.java
+++ plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/binance/v4/BinanceSwitcher.java
@@ -2,28 +2,40 @@ package io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4;
 
 import io.everytrade.server.model.TransactionType;
 
+import java.util.List;
+
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceConstantsV4.OPERATION_TYPE_BUY;
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceConstantsV4.OPERATION_TYPE_DEPOSIT;
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceConstantsV4.OPERATION_TYPE_DISTRIBUTION;
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceConstantsV4.OPERATION_TYPE_FEE;
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceConstantsV4.OPERATION_TYPE_FIAT_DEPOSIT;
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceConstantsV4.OPERATION_TYPE_FIAT_WITHDRAWAL;
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceConstantsV4.OPERATION_TYPE_SELL;
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceConstantsV4.OPERATION_TYPE_TRANSACTION_RELATED;
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceConstantsV4.OPERATION_TYPE_WITHDRAWAL;
+
 public class BinanceSwitcher {
 
     public static TransactionType operationTypeSwitcher(String operationType) {
-        if (BinanceConstantsV4.OPERATION_TYPE_BUY.code.equals(operationType)) {
+        if (OPERATION_TYPE_BUY.code.equals(operationType)) {
             return TransactionType.BUY;
         }
-        if (BinanceConstantsV4.OPERATION_TYPE_SELL.code.equals(operationType)) {
+        if (OPERATION_TYPE_SELL.code.equals(operationType)) {
             return TransactionType.SELL;
         }
-        if (BinanceConstantsV4.OPERATION_TYPE_DEPOSIT.code.equals(operationType)) {
+        if (List.of(OPERATION_TYPE_DEPOSIT.code, OPERATION_TYPE_FIAT_DEPOSIT.code).contains(operationType)) {
             return TransactionType.DEPOSIT;
         }
-        if (BinanceConstantsV4.OPERATION_TYPE_WITHDRAWAL.code.equals(operationType)) {
+        if (List.of(OPERATION_TYPE_WITHDRAWAL.code,OPERATION_TYPE_FIAT_WITHDRAWAL.code).contains(operationType)) {
             return TransactionType.WITHDRAWAL;
         }
-        if (BinanceConstantsV4.OPERATION_TYPE_FEE.code.equals(operationType)) {
+        if (OPERATION_TYPE_FEE.code.equals(operationType)) {
             return TransactionType.FEE;
         }
-        if (BinanceConstantsV4.OPERATION_TYPE_TRANSACTION_RELATED.code.equals(operationType)) {
+        if (OPERATION_TYPE_TRANSACTION_RELATED.code.equals(operationType)) {
             return TransactionType.BUY;
         }
-        if (BinanceConstantsV4.OPERATION_TYPE_DISTRIBUTION.code.equals(operationType)) {
+        if (OPERATION_TYPE_DISTRIBUTION.code.equals(operationType)) {
             return TransactionType.REWARD;
         }
         return TransactionType.UNKNOWN;
