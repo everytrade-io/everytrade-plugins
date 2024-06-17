5a59147fb6b321437f1f74a73101e6a61b6a4c28
MiroDoan <Slithercze@gmail.com>
2024-06-17 16:07:00 +0200

    bugfix/ETD-1297-containers-incorrect-transactio

--- plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/everytrade/EveryTradeBeanV3_2.java
+++ plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/everytrade/EveryTradeBeanV3_2.java
@@ -61,21 +61,21 @@ public class EveryTradeBeanV3_2 extends ExchangeBean {
     }
 
     @Parsed(field = "DATE")
-    @Format(formats = {
-        "dd.MM.yy",
-        "dd.MM.yy HH:mm",
-        "dd.MM.yy HH:mm:ss",
+    @Format(formats = { //the longest format should be always first
         "dd.MM.yyyy HH:mm:ss",
-        "dd/MM/yyyy",
+        "dd.MM.yy HH:mm:ss",
+        "dd.MM.yy HH:mm",
+        "dd.MM.yy",
         "dd/MM/yyyy HH:mm:ss",
-        "dd-MM-yyyy",
+        "dd/MM/yyyy",
         "dd-MM-yyyy HH:mm:ss",
-        "yyyy-MM-dd",
+        "dd-MM-yyyy",
         "yyyy-MM-dd HH:mm:ss",
-        "yyyy/MM/dd",
+        "yyyy-MM-dd",
         "yyyy/MM/dd HH:mm:ss",
-        "yyyy.MM.dd",
-        "yyyy.MM.dd HH:mm:ss"
+        "yyyy/MM/dd",
+        "yyyy.MM.dd HH:mm:ss",
+        "yyyy.MM.dd"
     },
         options = {"locale=US", "timezone=UTC"})
     public void setDate(Date value) {
