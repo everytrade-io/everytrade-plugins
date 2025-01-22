64a7c454ff724c6015943b529ed664bab29c261d
MiroDoan <Slithercze@gmail.com>
2025-01-22 15:16:23 +0100

    feature/ETD-39-connectors-supported-traded-curre-

--- plugin-api/src/main/java/io/everytrade/server/plugin/api/connector/ConnectorParameterType.java
+++ plugin-api/src/main/java/io/everytrade/server/plugin/api/connector/ConnectorParameterType.java
@@ -1,5 +1,5 @@
 package io.everytrade.server.plugin.api.connector;
 
 public enum ConnectorParameterType {
-    STRING, SECRET, BOOLEAN, FIAT_CURRENCY, LABEL
+    STRING, SECRET, BOOLEAN, FIAT_CURRENCY, LABEL, HIDDEN
 }
