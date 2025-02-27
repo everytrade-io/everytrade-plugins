a68a006db1af2bbf3c7fba936fddac075ce44f96
MiroDoan <Slithercze@gmail.com>
2025-02-27 15:13:14 +0100

    feature/ETD-1578-containers-change-invest-xlsx-s-

--- plugin-base/src/test/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/bean/TrezorSuiteBeanV1Test.java
+++ plugin-base/src/test/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/bean/TrezorSuiteBeanV1Test.java
@@ -21,7 +21,8 @@ public class TrezorSuiteBeanV1Test {
 
     @Test
     void testWithdrawal() {
-        final String row = "1694766966,15. 9. 2023,10:36:06 GMT+2,SENT,9471ff6309c3dd3e1d4f29b67fcb5bd5b7bc2f,0.0002629,BTC,bc1q09asldqlnzkjwvvtz9se3wuv,Affiliate ID 2733,0.00424137,BTC,105.87,\n";
+        final String row = "1694766966,15. 9. 2023,10:36:06 GMT+2,SENT,9471ff6309c3dd3e1d4f29b67fcb5bd5b7bc2f,0.0002629,BTC," +
+            "bc1q09asldqlnzkjwvvtz9se3wuv,Affiliate ID 2733,0.00424137,BTC,105.87,\n";
         final TransactionCluster actual1 = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
         final TransactionCluster expected1 = new TransactionCluster(
 
@@ -53,7 +54,8 @@ public class TrezorSuiteBeanV1Test {
 
     @Test
     void testDeposit() {
-        final String row = "1730877102,6. 11. 2024,8:11:42 GMT+1,RECV,c5da333de4fac7c04c23fd7496908423cbec81dc26b18eaa2,,,38jRfEf1MtDDKw71iwz,,15,BTC,\"1,019,318.33\",\n";
+        final String row = "1730877102,6. 11. 2024,8:11:42 GMT+1,RECV,c5da333de4fac7c04c23fd7496908423cbec81dc26b18eaa2,,," +
+            "38jRfEf1MtDDKw71iwz,,15,BTC,\"1,019,318.33\",\n";
         final TransactionCluster actual1 = ParserTestUtils.getTransactionCluster(HEADER_CORRECT + row);
         final TransactionCluster expected1 = new TransactionCluster(
 
@@ -75,7 +77,8 @@ public class TrezorSuiteBeanV1Test {
 
     @Test
     void testTransferOutIn() {
-        final String row = "1734612906,19. 12. 2024,13:55:06 GMT+1,SELF,a7bc556d2953e0469352ec594a1c4f774c298b56849e,0.0000083,BTC,34RtVhhernYn7TpDHn,,0.00001016,BTC,1,\n";
+        final String row = "1734612906,19. 12. 2024,13:55:06 GMT+1,SELF,a7bc556d2953e0469352ec594a1c4f774c298b56849e," +
+            "0.0000083,BTC,34RtVhhernYn7TpDHn,,0.00001016,BTC,1,\n";
         final var actual1 = ParserTestUtils.getTransactionClusters(HEADER_CORRECT + row);
 
         final TransactionCluster transferOut = new TransactionCluster(
