12562f60b5860cae265e2728f4869a2e268d2f60
Martin <89248739+martinkyov@users.noreply.github.com>
2025-08-27 13:29:34 +0200

    feature/ETD-1554-transactions-payment-type-trans (#519)

--- plugin-api/src/main/java/io/everytrade/server/model/TransactionType.java
+++ plugin-api/src/main/java/io/everytrade/server/model/TransactionType.java
@@ -17,7 +17,9 @@ public enum TransactionType {
     AIRDROP(11),
     EARNING(12),
     REWARD(13),
-    FORK(14)
+    FORK(14),
+    INCOMING_PAYMENT(15),
+    OUTGOING_PAYMENT(16)
     ;
 
     private final int code;
@@ -69,4 +71,13 @@ public enum TransactionType {
     public boolean isStaking() {
         return this == STAKE || this == UNSTAKE || this == STAKING_REWARD;
     }
+
+    public boolean isPayment() {
+        return this == INCOMING_PAYMENT || this == OUTGOING_PAYMENT;
+    }
+
+    public boolean isDepositOrWithdrawalOrPayment() {
+        return this == DEPOSIT || this == WITHDRAWAL || this == INCOMING_PAYMENT || this == OUTGOING_PAYMENT;
+    }
+
 }
