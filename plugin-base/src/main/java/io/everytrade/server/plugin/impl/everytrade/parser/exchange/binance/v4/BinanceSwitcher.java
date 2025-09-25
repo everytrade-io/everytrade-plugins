f93ce9545b5977124e3dff294d1978c512681755
Miroslav Doan <54637695+Slithercze@users.noreply.github.com>
2025-09-25 13:43:23 +0200

    feature/ETD-1563-imports-binance-csv-imports-upg- (#520)

--- plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/binance/v4/BinanceSwitcher.java
+++ plugin-base/src/main/java/io/everytrade/server/plugin/impl/everytrade/parser/exchange/binance/v4/BinanceSwitcher.java
@@ -2,7 +2,8 @@ package io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4;
 
 import io.everytrade.server.model.TransactionType;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.everytrade.server.model.TransactionType.AIRDROP;
 import static io.everytrade.server.model.TransactionType.BUY;
@@ -42,6 +43,7 @@ import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binanc
 import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceOperationTypeV4.OPERATION_TYPE_REFERRAL_COMMISSION;
 import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceOperationTypeV4.OPERATION_TYPE_SAVING_DISTRIBUTION;
 import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceOperationTypeV4.OPERATION_TYPE_SELL;
+import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceOperationTypeV4.OPERATION_TYPE_SELL_CRYPTO_TO_FIAT;
 import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceOperationTypeV4.OPERATION_TYPE_SIMPLE_EARN_FLEXIBLE_INTEREST;
 import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceOperationTypeV4.OPERATION_TYPE_SIMPLE_EARN_FLEXIBLE_REDEMPTION;
 import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binance.v4.BinanceOperationTypeV4.OPERATION_TYPE_SIMPLE_EARN_FLEXIBLE_SUBSCRIPTION;
@@ -63,64 +65,104 @@ import static io.everytrade.server.plugin.impl.everytrade.parser.exchange.binanc
 
 public class BinanceSwitcher {
 
-    public static TransactionType operationTypeSwitcher(String operationType) {
-        operationType = operationType.toUpperCase();
-        if (OPERATION_TYPE_BUY.code.equals(operationType) || OPERATION_TYPE_BINANCE_CONVERT.code.equals(operationType)
-              || OPERATION_TYPE_ETH2_0_STAKING.code.equals(operationType)) {
-            return BUY;
-        }
-        if (OPERATION_TYPE_SELL.code.equals(operationType) || OPERATION_TYPE_TRANSACTION_SPEND.code.equals(operationType)
-        || OPERATION_TYPE_TRANSACTION_SOLD.code.equals(operationType)) {
-            return SELL;
-        }
-        if (List.of(OPERATION_TYPE_DEPOSIT.code, OPERATION_TYPE_FIAT_DEPOSIT.code, OPERATION_TYPE_SIMPLE_EARN_FLEXIBLE_REDEMPTION.code,
-            OPERATION_TYPE_SAVING_DISTRIBUTION.code, OPERATION_TYPE_SIMPLE_EARN_FLEXIBLE_SUBSCRIPTION.code,
-            OPERATION_TYPE_SIMPLE_EARN_LOCKED_REDEMPTION.code, OPERATION_TYPE_DEPOSIT_FIAT_OCBS.code,
-            OPERATION_TYPE_BUY_CRYPTO_WITH_FIAT.code).contains(operationType)) {
-            return DEPOSIT;
-        }
-        if (List.of(OPERATION_TYPE_WITHDRAWAL.code, OPERATION_TYPE_FIAT_WITHDRAWAL.code, OPERATION_TYPE_FIAT_WITHDRAW.code,
-                OPERATION_TYPE_C2C_TRANSFER.code, OPERATION_TYPE_TRANSFER_BETWEEN_MAIN_ACC_AND_SUB_ACC.code)
-            .contains(operationType)) {
-            return WITHDRAWAL;
-        }
-        if (OPERATION_TYPE_FEE.code.equals(operationType) || OPERATION_TYPE_TRANSACTION_FEE.code.equals(operationType)
-            || OPERATION_TYPE_BNB_FEE_DEDUCTION.code.equals(operationType)) {
-            return FEE;
-        }
-        if (OPERATION_TYPE_TRANSACTION_RELATED.code.equals(operationType) || OPERATION_TYPE_LARGE_OTC_TRADING.code.equals(operationType)
-            || OPERATION_TYPE_SMALL_ASSETS_EXCHANGE_BNB.code.equals(operationType)
-            || OPERATION_TYPE_TRANSACTION_BUY.code.equals(operationType)
-            || OPERATION_TYPE_BUY_CRYPTO.code.equals(operationType)
-            || OPERATION_TYPE_TRANSACTION_REVENUE.code.equals(operationType)
-            || OPERATION_TYPE_CONVERT_FIAT_TO_CRYPTO_OCBS.code.equals(operationType)) {
-            return BUY;
-        }
-        if (OPERATION_TYPE_AIRDROP_ASSETS.code.equals(operationType) || OPERATION_TYPE_TOKEN_SWAP.code.equals(operationType)) {
-            return AIRDROP;
-        }
-        if (OPERATION_TYPE_DISTRIBUTION.code.equals(operationType) || OPERATION_TYPE_BNB_VAULT_REWARDS.code.equals(operationType)
-            || OPERATION_TYPE_REFERRAL_COMMISSION.code.equals(operationType)) {
-            return REWARD;
-        }
-        if (OPERATION_TYPE_COMMISSION_REBATE.code.equals(operationType) || OPERATION_TYPE_CARD_CASHBACK.code.equals(operationType)
-            || OPERATION_TYPE_CASHBACK_VOUCHER.code.equals(operationType)) {
-            return REBATE;
-        }
-        if (OPERATION_TYPE_SIMPLE_EARN_FLEXIBLE_INTEREST.code.equals(operationType)) {
-            return EARNING;
-        }
-        if (OPERATION_TYPE_STAKING_REWARDS.code.equals(operationType) || OPERATION_TYPE_ETH2_0_STAKING_REWARDS.code.equals(operationType)
-            || OPERATION_TYPE_SIMPLE_EARN_LOCKED_REWARDS.code.equals(operationType)) {
-            return STAKING_REWARD;
-        }
-        if (OPERATION_TYPE_STAKING_REDEMPTION.code.equals(operationType)) {
-            return UNSTAKE;
-        }
-        if (OPERATION_TYPE_STAKING_PURCHASE.code.equals(operationType)){
-            return STAKE;
-        }
-        return UNKNOWN;
+    private static final Map<String, TransactionType> OPERATION_MAP;
+
+    static {
+        Map<String, TransactionType> map = new HashMap<>();
+
+        putAll(map, BUY,
+            OPERATION_TYPE_BUY.code,
+            OPERATION_TYPE_BINANCE_CONVERT.code,
+            OPERATION_TYPE_ETH2_0_STAKING.code,
+            OPERATION_TYPE_BUY_CRYPTO_WITH_FIAT.code,
+            OPERATION_TYPE_TRANSACTION_RELATED.code,
+            OPERATION_TYPE_LARGE_OTC_TRADING.code,
+            OPERATION_TYPE_SMALL_ASSETS_EXCHANGE_BNB.code,
+            OPERATION_TYPE_TRANSACTION_BUY.code,
+            OPERATION_TYPE_BUY_CRYPTO.code,
+            OPERATION_TYPE_TRANSACTION_REVENUE.code,
+            OPERATION_TYPE_CONVERT_FIAT_TO_CRYPTO_OCBS.code
+        );
+
+        putAll(map, SELL,
+            OPERATION_TYPE_SELL.code,
+            OPERATION_TYPE_TRANSACTION_SPEND.code,
+            OPERATION_TYPE_TRANSACTION_SOLD.code,
+            OPERATION_TYPE_SELL_CRYPTO_TO_FIAT.code
+        );
+
+        putAll(map, DEPOSIT,
+            OPERATION_TYPE_DEPOSIT.code,
+            OPERATION_TYPE_FIAT_DEPOSIT.code,
+            OPERATION_TYPE_SIMPLE_EARN_FLEXIBLE_REDEMPTION.code,
+            OPERATION_TYPE_SAVING_DISTRIBUTION.code,
+            OPERATION_TYPE_SIMPLE_EARN_FLEXIBLE_SUBSCRIPTION.code,
+            OPERATION_TYPE_SIMPLE_EARN_LOCKED_REDEMPTION.code,
+            OPERATION_TYPE_DEPOSIT_FIAT_OCBS.code
+        );
+
+        putAll(map, WITHDRAWAL,
+            OPERATION_TYPE_WITHDRAWAL.code,
+            OPERATION_TYPE_FIAT_WITHDRAWAL.code,
+            OPERATION_TYPE_FIAT_WITHDRAW.code,
+            OPERATION_TYPE_C2C_TRANSFER.code,
+            OPERATION_TYPE_TRANSFER_BETWEEN_MAIN_ACC_AND_SUB_ACC.code
+        );
+
+        putAll(map, FEE,
+            OPERATION_TYPE_FEE.code,
+            OPERATION_TYPE_TRANSACTION_FEE.code,
+            OPERATION_TYPE_BNB_FEE_DEDUCTION.code
+        );
+
+        putAll(map, AIRDROP,
+            OPERATION_TYPE_AIRDROP_ASSETS.code,
+            OPERATION_TYPE_TOKEN_SWAP.code
+        );
+
+        putAll(map, REWARD,
+            OPERATION_TYPE_DISTRIBUTION.code,
+            OPERATION_TYPE_BNB_VAULT_REWARDS.code,
+            OPERATION_TYPE_REFERRAL_COMMISSION.code
+        );
+
+        putAll(map, REBATE,
+            OPERATION_TYPE_COMMISSION_REBATE.code,
+            OPERATION_TYPE_CARD_CASHBACK.code,
+            OPERATION_TYPE_CASHBACK_VOUCHER.code
+        );
+
+        putAll(map, EARNING,
+            OPERATION_TYPE_SIMPLE_EARN_FLEXIBLE_INTEREST.code
+        );
+
+        putAll(map, STAKING_REWARD,
+            OPERATION_TYPE_STAKING_REWARDS.code,
+            OPERATION_TYPE_ETH2_0_STAKING_REWARDS.code,
+            OPERATION_TYPE_SIMPLE_EARN_LOCKED_REWARDS.code
+        );
+
+        putAll(map, UNSTAKE,
+            OPERATION_TYPE_STAKING_REDEMPTION.code
+        );
+
+        putAll(map, STAKE,
+            OPERATION_TYPE_STAKING_PURCHASE.code
+        );
+
+        OPERATION_MAP = Map.copyOf(map);
     }
 
+    private static void putAll(Map<String, TransactionType> map, TransactionType type, String... ops) {
+        for (String op : ops) {
+            map.put(op, type);
+        }
+    }
+
+    public static TransactionType operationTypeSwitcher(String operationType) {
+        if (operationType == null) {
+            return UNKNOWN;
+        }
+        return OPERATION_MAP.getOrDefault(operationType.toUpperCase(), UNKNOWN);
+    }
 }
